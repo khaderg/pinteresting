0e1777e83862aa13273a8a7d0342f30683798ca0
Khader Ghanem <khadervghanem@gmail.com>
2015-07-29 19:24:49 +0300

    Add names to users

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -5,4 +5,7 @@ class Pin < ActiveRecord::Base
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 
  	validates_attachment_content_type :image, :content_type => [ 'image/gif', 'image/png', 'image/x-png', 'image/jpeg', 'image/pjpeg', 'image/jpg' ]
+
+  validates :image, presence: true
+  validates :description, presence: true
 end
